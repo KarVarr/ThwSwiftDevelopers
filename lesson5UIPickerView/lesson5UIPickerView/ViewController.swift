37650ce0af701083daf3fcd7bc0a2cbc8e2a900d
KarVarr <megatr9n@gmail.com>
2022-12-26 19:09:03 +0300

    added lesson # 5 UIPicker

---
+++ lesson5UIPickerView/lesson5UIPickerView/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  lesson5UIPickerView
+//
+//  Created by Karen Vardanian on 26.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let picker = UIPickerView()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        picker.center = view.center
+        view.addSubview(picker)
+    }
+
+
+}
+
