c88c9fc0d18666378f9fb89aa37281d921184172
KarVarr <megatr9n@gmail.com>
2022-12-28 20:55:12 +0300

    added lesson 8 UIActivityViewController

---
+++ lesson8UIActivityViewController/lesson8UIActivityViewController/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  lesson8UIActivityViewController
+//
+//  Created by Karen Vardanian on 28.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var buttonShare = UIButton()
+    var textField = UITextField()
+    var activityViewController: UIActivityViewController? = nil
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        
+    }
+
+    
+    func createTextField()  {
+        <#function body#>
+    }
+
+}
+
