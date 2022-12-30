0849f46706c65b431ac7ae35231bf0f7edd6fb96
KarVarr <megatr9n@gmail.com>
2022-12-30 20:05:49 +0300

    added UILabel and some style

--- lesson12UILabel/lesson12UILabel/ViewController.swift
+++ lesson12UILabel/lesson12UILabel/ViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var label : UILabel {
+        let label = UILabel()
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        label.text = "The Swift Developers"
+        label.font = UIFont.boldSystemFont(ofSize: 34)
+        label.textColor = .orange
+        label.shadowColor = .lightGray
+        label.shadowOffset = CGSize(width: 1, height: 1)
+        label.center = view.center
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.adjustsFontSizeToFitWidth = true
+        label.sizeToFit()
+        return label
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
+        view.addSubview(label)
     }
 
 
