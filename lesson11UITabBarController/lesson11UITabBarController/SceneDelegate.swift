847fde31ebde0cc957619773d6a4161d8c550a91
KarVarr <megatr9n@gmail.com>
2022-12-30 19:01:26 +0300

    added UITabBarItem, icons and titles

--- lesson11UITabBarController/lesson11UITabBarController/SceneDelegate.swift
+++ lesson11UITabBarController/lesson11UITabBarController/SceneDelegate.swift
@@ -13,9 +13,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let windowScene = scene as? UIWindowScene else  { return}
+        
+        self.window = UIWindow(windowScene: windowScene)
+        let firstVC = ViewController()
+        let secondVC = SecondViewController()
+        
+        let firstNavController = UINavigationController(rootViewController: firstVC)
+        let secondNavController = UINavigationController(rootViewController: secondVC)
+        
+        
+        let tabBarVC = UITabBarController()
+        tabBarVC.setViewControllers([firstNavController, secondNavController], animated: true)
+        
+        self.window?.rootViewController = tabBarVC
+        self.window?.makeKeyAndVisible()
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
--- lesson11UITabBarController/lesson11UITabBarController/SecondViewController.swift
+++ lesson11UITabBarController/lesson11UITabBarController/SecondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  lesson11UITabBarController
 //
 //  Created by Karen Vardanian on 30.12.2022.
@@ -7,26 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    
-    
+class SecondViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
         
-        navigationItem.title = "First VC"
+        navigationItem.title = "Second VC"
         var tabBarItem = UITabBarItem()
-        
-        tabBarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 0)
+        tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 1)
         self.tabBarItem = tabBarItem
         
-        view.backgroundColor = .orange
         
-       
     }
+    
 
 
 }
-
