35ef88bf8220ce8ba1df67e0fa6305857d37e73e
KarVarr <megatr9n@gmail.com>
2022-12-31 16:43:31 +0300

    added notificationCenter and touchBegan

--- lesson14UITextView/lesson14UITextView/ViewController.swift
+++ lesson14UITextView/lesson14UITextView/ViewController.swift
@@ -15,7 +15,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .orange
+        NotificationCenter.default.addObserver(self, selector: #selector(updateTextView(param:)), name: UIResponder.keyboardDidShowNotification, object: nil)
         
+        NotificationCenter.default.addObserver(self, selector: #selector(updateTextView(param:)), name: UIResponder.keyboardWillHideNotification, object: nil)
         createTextView()
         
     }
@@ -32,6 +34,22 @@ class ViewController: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         myTextView.resignFirstResponder()
         print("you click here")
+    }
+    
+    @objc func updateTextView(param: Notification) {
+        let userInfo = param.userInfo
+        
+        let getKeyboardRect = (userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardFrame = self.view.convert(getKeyboardRect, to: view.window)
+        
+        if param.name == UIResponder.keyboardWillHideNotification {
+            myTextView.contentInset = UIEdgeInsets.zero
+        } else {
+            myTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardFrame.height / 2, right: 0)
+            myTextView.scrollIndicatorInsets = myTextView.contentInset
+        }
+        
+        myTextView.scrollRangeToVisible(myTextView.selectedRange )
     }
     
     
