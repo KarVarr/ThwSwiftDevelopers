fb1d52857108ecd6e12d3547374d48c50cd59faa
KarVarr <megatr9n@gmail.com>
2023-01-06 19:33:37 +0300

    added square and my first animation

--- lesson21UIDynamicAnimator/lesson21UIDynamicAnimator/ViewController.swift
+++ lesson21UIDynamicAnimator/lesson21UIDynamicAnimator/ViewController.swift
@@ -8,11 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let squareView = UIView()
+    var animator = UIDynamicAnimator()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //make a square
+        squareView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        squareView.backgroundColor = .green
+        squareView.center = view.center
+        view.addSubview(squareView)
+        
+        //make an animation and gravity
+        animator = UIDynamicAnimator(referenceView: view)
+        let gravity = UIGravityBehavior(items: [squareView])
+        animator.addBehavior(gravity)
+    }
+    
+   
+    
 
 
 }
