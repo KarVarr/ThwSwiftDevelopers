53cf609f822b6242e7d4ed8d385e763b6d917bca
KarVarr <megatr9n@gmail.com>
2023-01-09 16:14:09 +0300

    added lesson 1 start

--- MyPlaygroundTheSwiftDevelopers.playground/Contents.swift
+++ MyPlaygroundTheSwiftDevelopers.playground/Contents.swift
@@ -1,3 +1,11 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var name1 = "alex"
+name1 = "karen"
+name1
+var name2 = "suzanna"
+print(name1 + " " + "dev" + " " + name2 + "  " + "teacher")
+
+//MARK: - lesson 2 base types
+
+
