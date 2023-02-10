ccea06b6154fe688915e0ed618b16266b16c3c35
KarVarr <megatr9n@gmail.com>
2023-02-10 18:23:27 +0300

    added MVP

---
+++ MVP-level One/MVP-level OneUITests/MVP_level_OneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MVP_level_OneUITestsLaunchTests.swift
+//  MVP-level OneUITests
+//
+//  Created by Karen Vardanian on 10.02.2023.
+//
+
+import XCTest
+
+final class MVP_level_OneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
