38e6ef6beaae390a228d1110a234e21af85ea082
KarVarr <megatr9n@gmail.com>
2024-08-17 13:00:14 +0400

    mvvmTest added struct Users

---
+++ mvvmTest/mvvmTest/Model/Users.swift
@@ -0,0 +1,20 @@
+//
+//  Users.swift
+//  mvvmTest
+//
+//  Created by Karen Vardanian on 17.08.2024.
+//
+
+import Foundation
+
+
+struct Users: Codable {
+    let name, username, email, phone: String
+    let address: Address
+}
+
+// MARK: - Address
+struct Address: Codable {
+    let street, suite, city: String
+}
+
