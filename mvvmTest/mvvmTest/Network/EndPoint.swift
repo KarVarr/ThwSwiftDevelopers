8cc11c6f4f76cce3b98bebc6aec317f5132c0453
KarVarr <megatr9n@gmail.com>
2024-08-17 12:57:02 +0400

    mvvmTest added struct EndPoin

--- mvvmTest/mvvmTest/Network/EndPoint.swift
+++ mvvmTest/mvvmTest/Network/EndPoint.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+
+
+struct EndPoint {
+    var path: String
+}
+
+extension EndPoint {
+    var url: URL {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "jsonplaceholder.typicode.com"
+        components.path = "/" + path
+        
+        guard let url = components.url else {
+            preconditionFailure("invalid URL components: \(components)")
+        }
+        return url
+    }
+}
+
+extension EndPoint {
+    static var users: Self {
+        EndPoint(path: "users")
+    }
+}
+
+
+// MARK: - WelcomeElement
+struct WelcomeElement: Codable {
+    let id: Int
+    let name, username, email: String
+    let address: Address
+    let phone, website: String
+    let company: Company
+}
+
+// MARK: - Address
+struct Address: Codable {
+    let street, suite, city, zipcode: String
+    let geo: Geo
+}
+
+// MARK: - Geo
+struct Geo: Codable {
+    let lat, lng: String
+}
+
+// MARK: - Company
+struct Company: Codable {
+    let name, catchPhrase, bs: String
+}
+
+typealias Welcome = [WelcomeElement]
