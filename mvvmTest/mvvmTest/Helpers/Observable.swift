7eaf4a94cd64af59990ccc24cfe3017e096d170d
KarVarr <megatr9n@gmail.com>
2024-08-17 13:58:41 +0400

    mvvmTest added observable

---
+++ mvvmTest/mvvmTest/Helpers/Observable.swift
@@ -0,0 +1,29 @@
+//
+//  Observable.swift
+//  mvvmTest
+//
+//  Created by Karen Vardanian on 17.08.2024.
+//
+
+import Foundation
+
+class Observable<T> {
+    var value: T? {
+        didSet {
+            DispatchQueue.main.async {
+                self.listener?(self.value)
+            }
+        }
+    }
+    
+    init(value: T? = nil) {
+        self.value = value
+    }
+    
+    private var listener: ((T?) -> Void)?
+    
+    func bind(_ listener: @escaping ((T?) -> Void)) {
+        listener(value)
+        self.listener = listener
+    }
+}
